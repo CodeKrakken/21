a711b8e61f14cf3a42aeebc69f5a75620781a377
CodeKrakken <newholm1@hotmail.com>
2021-02-08 13:22:36 +0000

    Draw random hand of two cards for player and dealer

--- runGame.js
+++ runGame.js
@@ -1,6 +1,10 @@
-console.log('game is running')
 const Deck = require('./lib/Deck')
 const Dealer = require('./lib/Dealer')
+const Player = require('./lib/Player')
+
 let deck = new Deck
 let dealer = new Dealer(deck)
-console.log(dealer)
+let sam = new Player
+// console.log(dealer)
+
+dealer.deal(sam)
