7f8e139aeec72c46e53c5c69ccef95def59cf53f
CodeKrakken <newholm1@hotmail.com>
2021-02-08 11:44:50 +0000

    Add deck of cards

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,3 +1,14 @@
 'use strict'
 
 const Deck = require('../lib/Deck.js')
+let deck
+
+describe('deck', function() {
+  beforeEach(function() {
+    deck = new Deck    
+  })
+
+  it('has an array of cards', function() {
+    expect(deck.cards).toBeDefined()
+  })
+})
